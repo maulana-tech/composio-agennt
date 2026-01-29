4214f786f77cddd83e21a0741b9f447abcec6a70
maulana-tech <firdaussyah03@gmail.com>
2026-01-29 11:31:54 +0700

    feat(ui): update components barrel file

---
+++ gmail-chatbot-ui/src/app/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Sidebar } from './Sidebar';
+export { default as HistorySidebar } from './HistorySidebar';
+export { default as ChatInput } from './ChatInput';
+export { default as ChatMessages } from './ChatMessages';
+export { default as WelcomeScreen } from './WelcomeScreen';
+export { default as AgentPanelLayout } from './AgentPanelLayout';
+export { default as Header } from './Header';
+export * from './icons';
